6a9c428c1670a2682655d1e764e89b031af1cafb
Toranova <chia_jason96@live.com>
2021-04-29 20:39:48 +0800

    working usbnet, TODO: fix buff overflow from esp_netif_recv

--- main/esp_usb_pppos_switch.c
+++ main/esp_usb_pppos_switch.c
@@ -6,9 +6,10 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+#define MEMHEAP_CHECK xPortGetFreeHeapSize
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE //debug
 
 #include <stdlib.h>
-#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE //debug
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
@@ -20,40 +21,36 @@
 #include "esp_eth.h"
 #include "esp_event.h"
 #include "sdkconfig.h"
+#include "netsuite_io.h"
+#include "httpd.h"
+
+//http server test
+//#include <esp_http_server.h>
 
 static const char *TAG_APP = "main";
 static const char *TAG_ETH = "usbeth";
 
-// Internal functions declaration referenced in io object
-void * netsuite_io_new(void);
-static esp_err_t netsuite_io_transmit(void *h, void *buffer, size_t len);
-static esp_err_t netsuite_io_transmit_wrap(void *h, void *buffer, size_t len, void *netstack_buf);
-static esp_err_t netsuite_io_attach(esp_netif_t * esp_netif, void * args);
-const esp_netif_driver_ifconfig_t driver_ifconfig = {
-        .driver_free_rx_buffer = NULL,
-        .transmit = netsuite_io_transmit,
-        .transmit_wrap = netsuite_io_transmit_wrap,
-        .handle = "netsuite-io-object" // this IO object is a singleton, its handle uses as a name
-};
-const esp_netif_driver_base_t s_driver_base = {
-        .post_attach =  netsuite_io_attach
-};
+//#include "lwip/netif.h"
+//#include "netif/ethernet.h"
+//extern void* esp_netif_get_netif_impl(esp_netif_t *esp_netif);
+
+//tinyusb network interface callbacks
+bool tud_network_recv_cb(const uint8_t *src, uint16_t size);
+uint16_t tud_network_xmit_cb(uint8_t *dst, void *ref, uint16_t arg);
+void tud_network_init_cb(void);
+void tud_service_traffic(void *pvparam);
 
 // usb descriptors
 extern tusb_desc_device_t desc_device;
 extern tusb_desc_strarray_device_t desc_string;
-// fp
-static struct pbuf *tud_rx_frame; //tud_rx_frame
-static void *tud_rx_buffer = NULL;
-static size_t tud_rx_bufsz;
-
-void tud_service_traffic(void *pvparam);
-bool tud_network_recv_cb(const uint8_t *src, uint16_t size);
-uint16_t tud_network_xmit_cb(uint8_t *dst, void *ref, uint16_t arg);
-void tud_network_init_cb(void);
+// gv
+static esp_netif_t* netif;
+static uint8_t tud_rx_buf[2048];
+static size_t tud_rx_len = 0;
 
 void app_main(void) {
     ESP_LOGI(TAG_APP, "tinyusb initialization.");
+    uint8_t tbuf[2048];
     tinyusb_config_t tusb_cfg = {
         .descriptor = &desc_device,
         .string_descriptor = desc_string,
@@ -78,98 +75,79 @@ void app_main(void) {
     ESP_ERROR_CHECK(esp_netif_str_to_ip4("255.255.255.0",&ip_info.netmask));
     ESP_ERROR_CHECK(esp_netif_str_to_ip4("0.0.0.0",&ip_info.gw));
 
-    // interface configuration
     esp_netif_inherent_config_t ihcfg = {
-            .flags = ESP_NETIF_FLAG_AUTOUP | ESP_NETIF_FLAG_GARP | ESP_NETIF_DHCP_SERVER,
-            .ip_info = (esp_netif_ip_info_t*)&ip_info,
-            .if_key = "USB",
-            .if_desc = "usbether"
+        //.flags = ESP_NETIF_FLAG_AUTOUP | ESP_NETIF_FLAG_GARP | ESP_NETIF_DHCP_SERVER,
+	.flags = (esp_netif_flags_t)(ESP_NETIF_FLAG_AUTOUP),
+        .ip_info = (esp_netif_ip_info_t*)&ip_info,
+        //.get_ip_event = 0,
+        //.lost_ip_event = 0,
+        .if_key = "USBNET",
+        .if_desc = "usb",
+        //.route_prio = 50
     };
+    // interface configuration
     esp_netif_config_t config = {
-        .base = &ihcfg,                          // use specific behaviour configuration
-        .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH, // use default ethernet-like network stack configuration
+        .base = &ihcfg,
+        .stack = ESP_NETIF_NETSTACK_DEFAULT_WIFI_STA,
+        //.driver = NULL,
     };
 
     // interface creation
     ESP_ERROR_CHECK(esp_netif_init());
-    esp_netif_t* netif = esp_netif_new(&config);
+    netif = esp_netif_new(&config);
     assert(netif);
     esp_netif_attach(netif, netsuite_io_new());
 
     // start netif
-    esp_netif_set_mac(netif, tud_virtual_mac_address);
+    ESP_ERROR_CHECK(esp_netif_set_mac(netif, tud_virtual_mac_address));
+    //ESP_ERROR_CHECK(esp_netif_set_ip_info(netif, &ip_info));
     esp_netif_action_start(netif, NULL, 0, NULL);
 
-    while (!esp_netif_is_netif_up(netif));
+    while (!esp_netif_is_netif_up(netif)) vTaskDelay( 100 / portTICK_PERIOD_MS );
     ESP_LOGI(TAG_APP, "usbnetif is up.");
 
-    //create the service task
-    xTaskCreate(&tud_service_traffic, "tud_service_traffic", 2048, netif, 10, NULL);
-}
+    //static httpd_handle_t server = NULL;
+    //start_webserver(); //start the server
 
-//TODO for all netsuite
-void * netsuite_io_new(void) {
-    return (void *)&s_driver_base;
-}
-
-static esp_err_t netsuite_io_attach(esp_netif_t * esp_netif, void * args) {
-    ESP_ERROR_CHECK(esp_netif_set_driver_config(esp_netif, &driver_ifconfig));
-    return ESP_OK;
-}
-
-static esp_err_t netsuite_io_transmit_wrap(void *h, void *buffer, size_t len, void *netstack_buf) {
-    return netsuite_io_transmit(h, buffer, len);
-}
-
-static esp_err_t netsuite_io_transmit(void *h, void *buffer, size_t len) {
-    for(;;){
-        if(!tud_ready()) return ESP_FAIL;
-	if(tud_network_can_xmit()){
-            struct pbuf *p = pbuf_alloc(PBUF_RAW, len, PBUF_POOL);
-	    memcpy(p->payload, buffer, len);
-	    tud_network_xmit(p, 0);
-	    return ESP_OK;
-	}
-    }
-}
-
-void tud_service_traffic(void *pvparam){
-    esp_netif_t* netif = (esp_netif_t*) pvparam;
-    // read from received_frame and pass the data to usb netif
-    while (1) {
-        if (tud_rx_buffer){ //received tud_rx
-            esp_netif_receive(netif, tud_rx_buffer, tud_rx_bufsz, NULL);
-            //free(tud_rx_buffer); // TODO: memleak problem
-            tud_rx_bufsz = 0;
-            tud_rx_buffer = NULL;
+    size_t hs;
+    while (1){
+        if (tud_rx_len > 0){
+            hs = MEMHEAP_CHECK();
+            //we have smth to read
+	    for(size_t i=0; i< tud_rx_len; i++){
+	        tbuf[i] = tud_rx_buf[i];
+	    }
+            esp_netif_receive(netif, tbuf, tud_rx_len, NULL);
             tud_network_recv_renew();
-            ESP_LOGD(TAG_APP, "tud_rx_buffer processed.");
+            tud_rx_len = 0; //indicate buf is now read
+	    ESP_LOGD(TAG_APP, "esp_netif_recv heap (%u).", hs);
         }else{
-	    vTaskDelay(10 / portTICK_RATE_MS);
-	}
+            vTaskDelay( 10 / portTICK_PERIOD_MS );
+        }
         sys_check_timeouts(); //TODO: is this important?
     }
+
 }
 
 bool tud_network_recv_cb(const uint8_t *src, uint16_t size) {
-    /* this shouldn't happen, but if we get another packet before
-       parsing the previous, we must signal our inability to accept it */
-    if (tud_rx_buffer) return false; //unable to process
-    if(size){
-        tud_rx_bufsz = size;
-	void * p = calloc(size, sizeof(uint8_t));
-        memcpy(p, src, tud_rx_bufsz);
-	tud_rx_buffer = p;
+    if (tud_rx_len < 1) {
+	    for(size_t i=0; i<size;i++){
+		    tud_rx_buf[i] = src[i];
+	    }
+	    //memcpy(tud_rx_buf, src, size);
+	    tud_rx_len = size; //set tud_rx_len ONLY after finish copying
+	    return true;
+    }else{
+	    //unable to receive, busy
+	    ESP_LOGW(TAG_ETH, "network_recv_cb busy.");
+	    return false;
     }
-    ESP_LOGD(TAG_ETH, "network_recv_cb.");
-    return true;
 }
 
 uint16_t tud_network_xmit_cb(uint8_t *dst, void *ref, uint16_t arg) {
     struct pbuf *p = (struct pbuf *)ref; //a linked list of pbufs
     struct pbuf *q;
     uint16_t len = 0;
-
     (void)arg; /* unused for this example */
     /* traverse the "pbuf chain"; see ./lwip/src/core/pbuf.c for more info */
     for(q = p; q != NULL; q = q->next) {
@@ -178,17 +156,11 @@ uint16_t tud_network_xmit_cb(uint8_t *dst, void *ref, uint16_t arg) {
         len += q->len; //increment the total length
         if (q->len == q->tot_len) break;
     }
-    ESP_LOGD(TAG_ETH, "network_xmit_cb.");
     return len; //return total length of the dst buffer
 }
 
 void tud_network_init_cb(void) {
     /* if the network is re-initializing and we have a leftover packet, we must do a cleanup */
-    if (tud_rx_frame) {
-	free(tud_rx_buffer);
-	tud_rx_bufsz = 0;
-        tud_rx_buffer= NULL;
-    }
     ESP_LOGD(TAG_ETH, "network_init_cb.");
     return;
 }
---
+++ main/httpd.h
@@ -0,0 +1,7 @@
+#ifndef __HTTPD_H__
+#define __HTTPD_H__
+
+#include <esp_http_server.h>
+httpd_handle_t start_webserver(void);
+
+#endif
